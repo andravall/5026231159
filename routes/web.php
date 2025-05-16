80101e46914b7584e2cdb60de3489d51a8c379f2
Ferdinand Valliandra <80774713+andravall@users.noreply.github.com>
2025-05-17 02:19:22 +0700

    Pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
+});
