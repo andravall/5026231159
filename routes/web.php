dcd453410d4d4b7b8ae2ebb06cd725aff62eda44
Ferdinand Valliandra <80774713+andravall@users.noreply.github.com>
2025-05-17 04:01:10 +0700

    Tugas Pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('halo', function () {
-	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
-});
+Route::view('/pertemuan3', 'pertemuan3');
+Route::view('/pertemuan4', 'pertemuan4');
+Route::view('/tugaslinktree', 'tugaslinktree');
+Route::view('/pertemuan7-js1', 'pertemuan7-js1');
+Route::view('/pertemuan7-js2', 'pertemuan7-js2');
+Route::view('/ets', 'ets');
